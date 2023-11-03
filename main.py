7d64d6baf38ed3ba351c41bebabeb492b796c000
M Abdul Fazal <mmohd.abdul.fazal@gmail.com>
2023-11-04 04:24:46 +0530

    changed to a better prompt

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import gradio
 with open('hidden.txt') as file:
     openai.api_key = file.read()
 
-messages = [{"role": "system", "content": 'You are a lawyer that is expert in IPR laws and cyber laws of India so that you can give advice to the client.'}]
+messages = [{"role": "system", "content": 'You are an Indian IPR and cyber Laws expert, you will generate answers to the queries raised by your client and help them understand what to do according to the information provided by the you client.You will also tell the client about the punishments acording to the sections of the indian IPR laws and cyber laws with the names of numbers of sections'}]
+
 
 def CustomChatGPT(Client_Query):
     messages.append({"role": "user", "content": Client_Query})
